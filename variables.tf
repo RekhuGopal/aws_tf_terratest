1915c85e1591eed62a4ec9e8988c7fccc6f925d5
RekhuGopal <vrchinnarathod@gmail.com>
2024-02-14 23:59:51 +0530

    test

--- variables.tf
+++ variables.tf
@@ -1,12 +1,33 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "with_policy" {
+  description = "If set to `true`, the bucket will be created with a bucket policy."
+  default = false
+}
+
 variable "tag_bucket_name" {
-  description = "Name for the S3 bucket"
+  description = "The Name tag to set for the S3 Bucket."
+  default     = "Test Bucket"
 }
 
 variable "tag_bucket_environment" {
-  description = "Environment for the S3 bucket"
-}
-
-variable "with_policy" {
-  description = "Flag indicating whether to include a policy"
-  type        = bool
+  description = "The Environment tag to set for the S3 Bucket."
+  default     = "Test"
 }
