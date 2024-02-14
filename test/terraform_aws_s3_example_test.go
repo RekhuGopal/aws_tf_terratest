a39df05b40c93e5a64d0fa843c4f8bdaa75266ac
RekhuGopal <vrchinnarathod@gmail.com>
2024-02-15 00:41:17 +0530

    Updated

--- test/terraform_aws_s3_example_test.go
+++ test/terraform_aws_s3_example_test.go
@@ -1,14 +1,9 @@
 package test
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -19,53 +14,32 @@ func TestTerraformAwsS3Example(t *testing.T) {
 
 	// Give this S3 Bucket a unique ID for a name tag so we can distinguish it from any other Buckets provisioned
 	// in your AWS account
-	expectedName := fmt.Sprintf("terratest-aws-s3-example-%s", strings.ToLower(random.UniqueId()))
+	//expectedName := fmt.Sprintf("terratest-aws-s3-example-%s", strings.ToLower(random.UniqueId()))
 
 	// Give this S3 Bucket an environment to operate as a part of for the purposes of resource tagging
-	expectedEnvironment := "AutomatedTesting"
+	//expectedEnvironment := "AutomatedTesting"
 
 	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
 	awsRegion := "us-west-2"
 
-	// Create terraform.auto.tfvars file
-	fileContent := fmt.Sprintf("tag_bucket_name = \"%s\"\ntag_bucket_environment = \"%s\"\nwith_policy = true", expectedName, expectedEnvironment)
-	err := os.WriteFile("terraform.auto.tfvars", []byte(fileContent), 0644)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Run terraform init
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "..", // Update this path to your Terraform configuration
+		TerraformDir: "..",
+
+		// Variables to pass to our Terraform code using -var options
+		VarFiles: []string{"terraform.auto.tfvars"},
 
 		// Environment variables to set when running Terraform
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": awsRegion,
-		},
-
-		// Variables to pass to our Terraform code using VAR=value environment variables
-		Vars: map[string]interface{}{
-			"tag_bucket_name":        expectedName,
-			"tag_bucket_environment": expectedEnvironment,
-			"with_policy":            true,
 		},
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply` using the terraform CLI
-	initAndApplyCmd := exec.Command("terraform", "init", "&&", "terraform", "apply", "-auto-approve")
-	initAndApplyCmd.Dir = terraformOptions.TerraformDir
-	initAndApplyCmd.Env = os.Environ()
-	initAndApplyCmd.Env = append(initAndApplyCmd.Env, fmt.Sprintf("AWS_DEFAULT_REGION=%s", awsRegion))
-
-	// Run the init and apply commands
-	output, err := initAndApplyCmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to run init and apply commands: %v\nOutput:\n%s", err, output)
-	}
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of an output variable
 	bucketID := terraform.Output(t, terraformOptions, "bucket_id")
