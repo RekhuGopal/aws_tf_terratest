905e5c1eaa221c24f9b4a58a24cc5b2cef1e4822
RekhuGopal <vrchinnarathod@gmail.com>
2024-02-14 22:47:47 +0530

    Updated

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@
 module "s3" {
   source                     = "./modules/s3"
   with_policy                = true
-  tag_bucket_name            = "module-eks-${random_string.suffix.result}"
+  tag_bucket_name            = "module-eks-${lower(random_string.suffix.result)}"
   tag_bucket_environment     = "Dev"
 }
